f994c78b24219329244beae069948a3970e44a9f
zeluliCredit <zeluli@creditease.cn>
2016-07-11 18:32:36 +0800

    code update

--- CECustomeSegmentControl/CECustomeSegmentControl/NoUserMaskViewController.swift
+++ CECustomeSegmentControl/CECustomeSegmentControl/NoUserMaskViewController.swift
@@ -12,34 +12,55 @@ class NoUserMaskViewController: UIViewController {
     
     var grayImageView: UIImageView!
     var colorImageView: UIImageView!
+    var maskView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addGrayImageView()
-        addColorImageView()
+        addMaskView()
     }
     
     func addGrayImageView() {
         grayImageView = UIImageView(frame: self.view.bounds)
         grayImageView.backgroundColor = UIColor.init(patternImage: UIImage(named: "gray")!)
-        self.addImageView(grayImageView, imageName: GrayImageName)
+        grayImageView.contentMode = .ScaleAspectFit
+        grayImageView.image = UIImage(named: GrayImageName)
+        self.view.addSubview(grayImageView)
+    }
+    
+    func addMaskView() {
+        let maskFrame = CGRect(origin: CGPointZero, size: CGSizeMake(100, 100))
+        maskView = UIView(frame: maskFrame)
+        maskView.center = self.view.center
+        maskView.layer.cornerRadius = 50
+        maskView.clipsToBounds = true
+        maskView.backgroundColor = UIColor.init(patternImage: UIImage(named: "color")!)
+        self.view.addSubview(maskView)
+        self.addColorImageView()
     }
     
     func addColorImageView() {
-        colorImageView = UIImageView(frame: self.view.bounds)
-        colorImageView.backgroundColor = UIColor.init(patternImage: UIImage(named: "color")!)
-        self.addImageView(colorImageView, imageName: ColorImageName)
+        let colorImageFrame = CGRect(origin: CGPointZero, size: self.view.bounds.size)
+        colorImageView = UIImageView(frame: colorImageFrame)
+        colorImageView.center = CGPointMake(50, 50)
+        colorImageView.contentMode = .ScaleAspectFit
+        colorImageView.image = UIImage(named: ColorImageName)
+        self.maskView.addSubview(colorImageView)
     }
     
-    func addImageView(imageView: UIImageView, imageName: String) {
-        imageView.contentMode = .ScaleAspectFit
-        imageView.image = UIImage(named: imageName)
-        self.view.addSubview(imageView)
-    }
+//    func countColorImageViewCenter() -> CGPoint {
+//        
+//    }
     
+
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touche in touches {
-            print(touche.locationInView(self.view))
+            let maskCenter = touche.locationInView(self.view)
+            let colorImageCenter = touche.locationInView(self.maskView)
+            print(maskCenter)
+            print(colorImageCenter)
+            self.maskView.center = maskCenter
         }
     }
 
